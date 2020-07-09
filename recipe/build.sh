bac8b4ee221878262c585a5348d933fcd06fb940
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-07-09 14:13:15 +0200

    Tests exceed Travis time - disable :(

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ cmake \
     -D PYTHON_INSTDIR="$SP_DIR" \
     -D RDK_BUILD_AVALON_SUPPORT=ON \
     -D RDK_BUILD_CAIRO_SUPPORT=ON \
-    -D RDK_BUILD_CPP_TESTS=ON \
+    -D RDK_BUILD_CPP_TESTS=OFF \
     -D RDK_BUILD_INCHI_SUPPORT=ON \
     -D RDK_BUILD_FREESASA_SUPPORT=ON \
     -D RDK_BUILD_YAEHMOP_SUPPORT=ON \
@@ -23,7 +23,8 @@ cmake \
 make -j$CPU_COUNT
 make install
 
-## Remove before merge
-#  Remember to set RDK_BUILD_CPP_TESTS back to OFF
-export RDBASE="$SRC_DIR"
-ctest --output-on-failure
+## How to run unit tests:
+## 1. Set RDK_BUILD_CPP_TESTS to ON
+## 2. Uncomment lines below
+# export RDBASE="$SRC_DIR"
+# ctest --output-on-failure
