2f0db212b5687ef987f67721c95793939e336b79
Greg Landrum <greg.landrum@gmail.com>
2021-11-02 12:58:56 +0100

    copy DLLs

--- recipe/install_rdkit_dev.bat
+++ recipe/install_rdkit_dev.bat
@@ -6,6 +6,9 @@ if not exist "%LIBRARY_INC%" mkdir %LIBRARY_INC%
 REM copy .lib files to LIBRARY_LIB
 copy lib\*.lib %LIBRARY_LIB%
 
+REM copy .dll files to LIBRARY_LIB
+copy lib\*.dll %LIBRARY_LIB%
+
 REM copy .h files to LIBRARY_INC
 mkdir %LIBRARY_INC%\rdkit
 xcopy /y /s Code\*.h %LIBRARY_INC%\rdkit
