23cd33e4baf5e7b5cea51f630e578832a449a1dd
Greg Landrum <greg.landrum@gmail.com>
2021-11-06 04:19:24 +0100

    tracking down copy problems

--- recipe/install_rdkit_dev.bat
+++ recipe/install_rdkit_dev.bat
@@ -3,10 +3,13 @@ echo "Copying libs and headers"
 if not exist "%LIBRARY_LIB%" mkdir %LIBRARY_LIB%
 if not exist "%LIBRARY_INC%" mkdir %LIBRARY_INC%
 
+REM what's in the lib directory?
+dir lib
+
 REM copy .lib files to LIBRARY_LIB
 copy lib\*.lib %LIBRARY_LIB%
 
-REM copy .dll files to LIBRARY_LIB
+REM copy .dll files to LIBRARY_BIN
 if not exist "%LIBRARY_BIN%" mkdir %LIBRARY_BIN%
 copy lib\*.dll %LIBRARY_BIN%
 
