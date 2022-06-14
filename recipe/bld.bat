147078339455a62b18befa996a2d98bab4f69e63
Nicolas ELIE <nicolas.elie@cnrs.fr>
2022-06-14 16:09:50 +0200

    Remove rdkit-dev

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -29,3 +29,14 @@ if errorlevel 1 exit 1
 REM copy .dll files to LIBRARY_BIN
 copy bin\*.dll %LIBRARY_BIN%
 
+REM copy .h files to LIBRARY_INC
+mkdir %LIBRARY_INC%\rdkit
+xcopy /y /s Code\*.h %LIBRARY_INC%\rdkit
+
+REM copy external .h files to LIBRARY_INC
+xcopy /y External\INCHI-API\*.h %LIBRARY_INC%\rdkit\GraphMol
+xcopy /y External\AvalonTools\*.h %LIBRARY_INC%\rdkit\GraphMol
+xcopy /y External\FreeSASA\*.h %LIBRARY_INC%\rdkit\GraphMol
+xcopy /y External\CoordGen\*.h %LIBRARY_INC%\rdkit\GraphMol
+xcopy /y External\YAeHMOP\*.h %LIBRARY_INC%\rdkit\GraphMol
+xcopy /y External\RingFamilies\RingDecomposerLib\src\RingDecomposerLib\RingDecomposerLib.h %LIBRARY_INC%\rdkit
