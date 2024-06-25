3dce9cd7204e2caa593e3e0f441c479f7c2bf6ea
Steven Kearnes <skearnes@relaytx.com>
2024-06-25 15:49:33 -0400

    pg_config

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 POPCNT_OPTIMIZATION="ON"
-if [[ "$target_platform" == linux-ppc64le || "$target_platform" == linux-aarch64 ]]; then
+if [ "${target_platform}" == "linux-ppc64le" ] || [ "${target_platform}" == "linux-aarch64" ]; then
     # PowerPC includes -mcpu=power8 optimizations already
     # ARM does not have popcnt instructions, afaik
     POPCNT_OPTIMIZATION="OFF"
@@ -11,22 +11,22 @@ fi
 
 # Numpy cannot be found in ppc64le for some reason... some extra help will do ;)
 EXTRA_CMAKE_FLAGS=""
-if [[ "$target_platform" == linux-ppc64le ]]; then
+if [ "${target_platform}" == "linux-ppc64le" ]; then
     EXTRA_CMAKE_FLAGS+=" -D PYTHON_NUMPY_INCLUDE_PATH=${SP_DIR}/numpy/core/include"
 fi
 
 # workaround for clang++ and boost functional
-if [[ "$target_platform" == osx-arm64 ]] || [[ "$target_platform" == osx-64 ]]; then
+if [ "${target_platform}" == "osx-arm64" ] || [ "${target_platform}" == "osx-64" ]; then
     export CXXFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY -D_HAS_AUTO_PTR_ETC=0 $CXXFLAGS"
 fi
 
 cmake ${CMAKE_ARGS} \
     -D CMAKE_BUILD_TYPE=Release \
-    -D CMAKE_INSTALL_PREFIX="$PREFIX" \
-    -D BOOST_ROOT="$PREFIX" \
+    -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -D BOOST_ROOT="${PREFIX}" \
     -D Boost_NO_SYSTEM_PATHS=ON \
     -D Boost_NO_BOOST_CMAKE=ON \
-    -D PYTHON_EXECUTABLE="$PYTHON" \
+    -D PYTHON_EXECUTABLE="${PYTHON}" \
     -D RDK_BUILD_AVALON_SUPPORT=ON \
     -D RDK_BUILD_CAIRO_SUPPORT=ON \
     -D RDK_BUILD_CPP_TESTS=OFF \
@@ -36,13 +36,14 @@ cmake ${CMAKE_ARGS} \
     -D RDK_BUILD_XYZ2MOL_SUPPORT=ON \
     -D RDK_INSTALL_INTREE=OFF \
     -D RDK_INSTALL_STATIC_LIBS=OFF \
-    -D RDK_OPTIMIZE_POPCNT=${POPCNT_OPTIMIZATION} \
+    -D RDK_OPTIMIZE_POPCNT="${POPCNT_OPTIMIZATION}" \
     -D RDK_BUILD_PGSQL=ON \
     -D RDK_PGSQL_STATIC=OFF \
+    -D PostgreSQL_ROOT="${PREFIX}" \
     ${EXTRA_CMAKE_FLAGS} \
     .
 
-make -j$CPU_COUNT
+make -j"${CPU_COUNT}"
 
 ## How to run unit tests:
 ## 1. Set RDK_BUILD_CPP_TESTS to ON
--- recipe/install.sh
+++ recipe/install.sh
@@ -2,14 +2,14 @@
 
 set -euxo pipefail
 
-if [[ $PKG_NAME == "librdkit" ]]; then
+if [ "${PKG_NAME}" == "librdkit" ]; then
 
     # NOTE(skearnes): List is from `make list_install_components`, excluding "python" and "pgsql".
     for COMPONENT in Unspecified base data dev docs extras runtime; do
       cmake -D CMAKE_INSTALL_COMPONENT="${COMPONENT}" -P cmake_install.cmake
     done
 
-elif [[ $PKG_NAME == "rdkit" ]]; then
+elif [ "${PKG_NAME}" == "rdkit" ]; then
 
     cmake -D CMAKE_INSTALL_COMPONENT=python -P cmake_install.cmake
     cmake --build . --config Release --target stubs
@@ -18,18 +18,18 @@ elif [[ $PKG_NAME == "rdkit" ]]; then
     # so python can correctly detect whether rdkit is installed when it's coming from a conda package.
 
     # Set the version for setuptools_scm.
-    echo "Setting SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION"
-    export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
+    echo "Setting SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION}"
+    export SETUPTOOLS_SCM_PRETEND_VERSION="${PKG_VERSION}"
 
     # Install the Python library.
     ${PYTHON} -m pip install --no-deps -vv --no-build-isolation --prefix "${PREFIX}" .
 
-elif [[ $PKG_NAME == "rdkit-postgresql" ]]; then
+elif [ "${PKG_NAME}" == "rdkit-postgresql" ]; then
 
-    if [ "$(uname)" == "Darwin" ]; then
+    if [ "${target_platform}" == "osx-arm64" ] || [ "${target_platform}" == "osx-64" ]; then
       # NOTE(skearnes): PostgreSQL doesn't recognize `rdkit.so` on OSX.
-      DIR=$(pg_config --pkglibdir)
-      ln -s "$DIR"/rdkit.so "$DIR"/rdkit
+      DIR="$(pg_config --pkglibdir)"
+      ln -s "${DIR}"/rdkit.so "${DIR}"/rdkit
     fi
     cd ./Code/PgSQL/rdkit
     cmake -P cmake_install.cmake
