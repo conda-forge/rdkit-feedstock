bba3394b2ddb7780b96ef5bc6e5117592b47cdcf
Steven Kearnes <skearnes@gmail.com>
2024-06-29 18:04:05 -0400

    alphabetical options

--- recipe/build.sh
+++ recipe/build.sh
@@ -26,26 +26,26 @@ if [ "${target_platform}" == "osx-arm64" ]; then
   chmod +x "${RECIPE_DIR}/arm64_pg_config"
   PG_CONFIG="${RECIPE_DIR}/arm64_pg_config"
 fi
-time cmake ${CMAKE_ARGS} --trace-expand --trace-source=Code/PgSQL/rdkit/CMakeLists.txt \
-    -D CMAKE_BUILD_TYPE=Release \
-    -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -D BOOST_ROOT="${PREFIX}" \
+time cmake ${CMAKE_ARGS} \
     -D Boost_NO_SYSTEM_PATHS=ON \
     -D Boost_NO_BOOST_CMAKE=ON \
+    -D BOOST_ROOT="${PREFIX}" \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -D PostgreSQL_CONFIG="${PG_CONFIG}" \
     -D PYTHON_EXECUTABLE="${PYTHON}" \
     -D RDK_BUILD_AVALON_SUPPORT=ON \
     -D RDK_BUILD_CAIRO_SUPPORT=ON \
     -D RDK_BUILD_CPP_TESTS=OFF \
-    -D RDK_BUILD_INCHI_SUPPORT=ON \
     -D RDK_BUILD_FREESASA_SUPPORT=ON \
-    -D RDK_BUILD_YAEHMOP_SUPPORT=ON \
+    -D RDK_BUILD_INCHI_SUPPORT=ON \
+    -D RDK_BUILD_PGSQL=ON \
     -D RDK_BUILD_XYZ2MOL_SUPPORT=ON \
+    -D RDK_BUILD_YAEHMOP_SUPPORT=ON \
     -D RDK_INSTALL_INTREE=OFF \
     -D RDK_INSTALL_STATIC_LIBS=OFF \
     -D RDK_OPTIMIZE_POPCNT="${POPCNT_OPTIMIZATION}" \
-    -D RDK_BUILD_PGSQL=ON \
     -D RDK_PGSQL_STATIC=OFF \
-    -D PostgreSQL_CONFIG="${PG_CONFIG}" \
     ${EXTRA_CMAKE_FLAGS} \
     .
 
