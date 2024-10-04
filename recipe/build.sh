3f5c9944050503ee5d346c6f3ad35e0c90567b2c
Lori A. Burns <lori.burns@gmail.com>
2024-10-04 14:53:53 -0400

    Update recipe/build.sh
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ if [ "${target_platform}" == "linux-ppc64le" ] || [ "${target_platform}" == "lin
     POPCNT_OPTIMIZATION="OFF"
 fi
 
-EXTRA_CMAKE_FLAGS=" -D Python3_NumPy_INCLUDE_DIR=$(python -c 'import numpy as np; print(np.get_include())'"
+EXTRA_CMAKE_FLAGS=" -D Python3_NumPy_INCLUDE_DIR=$(python -c 'import numpy as np; print(np.get_include())')"
 
 
 PG_CONFIG="$(which pg_config)"
